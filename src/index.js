2a15d21b60a6b33c0d98ec194691e40aeff97696
akerpelm <akerpelm@u.rochester.edu>
2022-08-28 11:24:09 +0200

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 // import 'semantic-ui-css/semantic.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
